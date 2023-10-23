b16810b571c4c47be0c14d56e380755661e7da00
yuto ichinose <yuto.0526.sano@gmail.com>
2023-10-23 14:38:29 +0900

    feat: #1 #2 キーワードの保存と呼び出し機能実装

---
+++ src/lib/memoKeywords.ts
@@ -0,0 +1,45 @@
+/**
+ * キーワードを保存
+ * @param route ルート
+ * @param key キーワード
+ */
+export const saveKeyword = (route: string, key: string) => {
+  // 先に取得
+  const data = localStorage.getItem(`keyword${route}`);
+  // 配列なら格納、データなしならそのまま新しく配列に保存
+  if (data && isJson(data)) {
+    const arr:string[] = JSON.parse(data);
+    const newArr = arr.includes(key) ? arr : [...arr, key];
+    localStorage.setItem(`keyword${route}`, JSON.stringify(newArr));
+  } else {
+    const newArr = [key];
+    localStorage.setItem(`keyword${route}`, JSON.stringify(newArr));
+  }
+};
+
+/**
+ * キーワード読み込み
+ * 
+ * A ~ Eを繰り返しで呼び出すようにする
+ * 
+ * @param route ルート
+ * @returns データ
+ */
+export const loadKeyword = (route: string) => {
+    const data = localStorage.getItem(`keyword${route}`)
+    if (data && isJson(data)) {
+        const arr: string[] = JSON.parse(data)
+        return arr
+    } else {
+        return []
+    }
+};
+
+const isJson = (text: string) => {
+  try {
+    JSON.parse(text);
+    return true;
+  } catch (error) {
+    return false;        
+  }
+};
