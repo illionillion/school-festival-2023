ee4602e13af879aa1b119c42363453e7e537da27
yuto ichinose <yuto.0526.sano@gmail.com>
2023-10-25 00:01:10 +0900

    fix: 遷移用のリンク追加

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,9 @@
-import { Box, Heading } from '@chakra-ui/react';
+import { Flex, Heading, Link } from '@chakra-ui/react';
 import type { FC } from 'react';
 
 export const Header:FC = () => {
-  return <Box as='header' w='full' background='#47BED8' px='2.5' py='5' position='sticky' top={0}>
+  return <Flex as='header' w='full' background='#47BED8' px='2.5' py='5' position='sticky' align='center' gap={5} top={0}>
     <Heading color='#fff' fontSize='2xl' fontWeight='medium'>NFC謎解き</Heading>
-  </Box>;
+    <Link color="#fff" href='/keyword/list'>メモ一覧</Link>
+  </Flex>;
 };
