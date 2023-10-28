bdafea877f860af997b28ffe0fd501e993fbe3bb
epc11 <epc1126@yahoo.co.jp>
2023-10-28 22:38:13 +0900

    header長押し時の選択も無効になるように変更

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,7 +3,7 @@ import type { FC } from 'react';
 
 export const Header:FC = () => {
   return <Flex as='header' w='full' background='#47BED8' px='2.5' py='5' position='sticky' align='center' gap={5} top={0}>
-    <Heading color='#fff' fontSize='2xl' fontWeight='medium'>NFC謎解き</Heading>
-    <Link color="#fff" href='/keyword/list'>メモ一覧</Link>
+    <Heading color='#fff' fontSize='2xl' fontWeight='medium' userSelect={'none'}>NFC謎解き</Heading>
+    <Link color="#fff" href='/keyword/list' userSelect={'none'}>メモ一覧</Link>
   </Flex>;
 };
