998a21841e0ea7c61e1d64d6b133b63b82f348e6
epc11 <epc1126@yahoo.co.jp>
2023-10-29 22:20:48 +0900

    #10

--- src/components/saveKeywordList/index.tsx
+++ src/components/saveKeywordList/index.tsx
@@ -25,17 +25,17 @@ export const SaveKeywordList:FC = () => {
     );
   },[]);
   return <VStack w='100vw' h="calc(100svh - 72px)" justify='center' alignItems='center'>
-      <Text fontSize='2xl' userSelect="none">キーワードメモ</Text>
-      {words.map((item, index) => (
-        <Box key={index}>
-          <Text userSelect="none">ルート：{item.route}</Text>
-          <UnorderedList userSelect="none">
-            {item.keywords.map((word, index) => (
-              <ListItem key={index}>{word}</ListItem>
-            ))}
-          </UnorderedList>
-        </Box>
-      ))}
+    <Text fontSize='2xl' userSelect="none">キーワードメモ</Text>
+    {words.map((item, index) => (
+      <Box key={index}>
+        <Text userSelect="none">ルート：{item.route}</Text>
+        <UnorderedList userSelect="none">
+          {item.keywords.map((word, index) => (
+            <ListItem key={index}>{word}</ListItem>
+          ))}
+        </UnorderedList>
+      </Box>
+    ))}
     <Button colorScheme="red" onClick={resetKeywords}>リセット</Button>
   </VStack>;
 };
