7d9330da02ca4c5d087ffc7da5060df48654cb59
epc11 <epc1126@yahoo.co.jp>
2023-10-29 17:39:59 +0900

    キーワードをタップすると大きくなるように変更

--- src/components/keywordScreen/index.tsx
+++ src/components/keywordScreen/index.tsx
@@ -30,7 +30,7 @@ export const KeywordScreen: FC<KeywordScreenProps> = ({ route, keyword }) => {
           borderStyle='solid'
           margin='auto'
         >
-        <motion.div animate={{ y: 0 }} transition={{ type: 'spring'}} initial={{ y: '-15vh' }}>
+        <motion.div animate={{ y: 0 }} transition={{ type: 'spring'}} initial={{ y: '-15vh' }} whileTap={{  }}>
           <Text fontSize='9xl' userSelect="none">{keyword}</Text>
         </motion.div>
         </Center>
