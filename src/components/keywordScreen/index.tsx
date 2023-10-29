d4e8acbd0fee9a24494ac054d2fe6b1995d8a144
epc11 <epc1126@yahoo.co.jp>
2023-10-29 19:17:33 +0900

    ページを読み込んだ時に全てがフェードインするように変更

--- src/components/keywordScreen/index.tsx
+++ src/components/keywordScreen/index.tsx
@@ -12,34 +12,36 @@ export const KeywordScreen: FC<KeywordScreenProps> = ({ route, keyword }) => {
   };
   return (<>
     <VStack w='100vw' h="calc(100svh - 72px)" justify='center' alignItems='center' py='14'>
-      <VStack gap={3}>
-        <Center gap={3}>
-          <Text fontSize='2xl' userSelect="none">ルート</Text>
-          <Text fontSize='2xl' color='red' userSelect="none">{route}</Text>
-        </Center>
-        <Center fontSize='2xl' userSelect="none">キーワード</Center>
-      </VStack>
-      <Box py='6'>
-        <Center
-          maxW='356px'
-          maxH='356px'
-          w="calc(100vw * 0.75)"
-          h="calc(100vw * 0.75)"
-          borderWidth={1}
-          borderColor='#1188AE'
-          borderStyle='solid'
-          margin='auto'
-        >
-        <motion.div animate={{ y: 0 }} transition={{ type: 'spring' }} initial={{ y: '-15vh' }} whileTap={{ scale: 0.8, rotate: 360 }}>
-          <Text fontSize='9xl' userSelect="none">{keyword}</Text>
+      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+          <VStack gap={3}>
+            <Center gap={3}>
+              <Text fontSize='2xl' userSelect="none">ルート</Text>
+              <Text fontSize='2xl' color='red' userSelect="none">{route}</Text>
+            </Center>
+            <Center fontSize='2xl' userSelect="none">キーワード</Center>
+          </VStack>
+        <Box py='6'>
+          <Center
+            maxW='356px'
+            maxH='356px'
+            w="calc(100vw * 0.75)"
+            h="calc(100vw * 0.75)"
+            borderWidth={1}
+            borderColor='#1188AE'
+            borderStyle='solid'
+            margin='auto'
+          >
+          <motion.div animate={{ y: 0 }} transition={{ type: 'spring' }} initial={{ y: '-15vh' }} whileTap={{ scale: 0.8, rotate: 360 }}>
+            <Text fontSize='9xl' userSelect="none">{keyword}</Text>
+          </motion.div>
+          </Center>
+        </Box>
+        <Center>
+        <motion.div whileTap={{ scale: 0.8 }}>
+          <Button px='10' py='7' colorScheme="blue" borderRadius={15} onClick={handleSave}>メモする</Button>
         </motion.div>
         </Center>
-      </Box>
-      <Center>
-      <motion.div whileTap={{ scale: 0.8 }}>
-        <Button px='10' py='7' colorScheme="blue" borderRadius={15} onClick={handleSave}>メモする</Button>
       </motion.div>
-      </Center>
     </VStack>
   </>
   );
